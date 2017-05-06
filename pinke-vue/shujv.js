422af4bd8ef259369f23818cf6d889f0e4ce4c47
于谦益 <yuqianyi@yuqianyideMacBook-Pro.local>
2017-05-06 16:38:07 +0800

    片刻1.0

---
+++ pinke-vue/shujv.js
@@ -0,0 +1,125 @@
+var article = [
+    {
+        title: '你说的都对，但……「理都懂」的背后，是套路满满的心理',
+        summary: '「理都懂」的背后，是套路满满的心理。',
+        uname: '简单心理',
+        views: '2345',
+        likes: '214',
+        comments: '24',
+        cover: 'images/article/56d40ff36cd746ee317cfc63a259ab5f20170427.jpg'
+    },
+    {
+        title: '你说的都对，但……「理都懂」的背后，是套路满满的心理',
+        summary: '「理都懂」的背后，是套路满满的心理。',
+        uname: '简单心理',
+        views: '2345',
+        likes: '214',
+        comments: '24',
+        cover: 'images/article/56d40ff36cd746ee317cfc63a259ab5f20170427.jpg'
+    },
+    {
+        title: '你说的都对，但……「理都懂」的背后，是套路满满的心理',
+        summary: '「理都懂」的背后，是套路满满的心理。',
+        uname: '简单心理',
+        views: '2345',
+        likes: '214',
+        comments: '24',
+        cover: 'images/article/56d40ff36cd746ee317cfc63a259ab5f20170427.jpg'
+    }
+];
+
+
+var ting = [
+    {
+        title: '「四海八荒 三生痴狂」《三生三世》',
+        summary: '只愿为你三生痴狂',
+        uname: '任路加',
+        views: '2345',
+        likes: '214',
+        comments: '24',
+        cover: 'images/ting/ff83d093806e4aa97d7579c27ecb90e120170228.jpg'
+    },
+    {
+        title: '「四海八荒 三生痴狂」《三生三世》',
+        summary: '只愿为你三生痴狂',
+        uname: '任路加',
+        views: '2345',
+        likes: '214',
+        comments: '24',
+        cover: 'images/ting/ff83d093806e4aa97d7579c27ecb90e120170228.jpg'
+    },
+    {
+        title: '「四海八荒 三生痴狂」《三生三世》',
+        summary: '只愿为你三生痴狂',
+        uname: '任路加',
+        views: '2345',
+        likes: '214',
+        comments: '24',
+        cover: 'images/ting/ff83d093806e4aa97d7579c27ecb90e120170228.jpg'
+    }
+];
+
+var vision = [
+    {
+        title: '绘画教程 | 冰淇淋',
+        summary: '满足你的少女心，一支冰淇淋就够了。',
+        uname: '爱蜜部落',
+        views: '2345',
+        likes: '214',
+        comments: '24',
+        cover: [
+            'images/vision/6c696f262f8e49c3f917c77b34b4d27920170315.jpg',
+            'images/vision/986195f228cd6690984783bfdf9e091d20170315.jpg',
+            'images/vision/e4e390d2846c30455528358fdc9619d820170315.jpg'
+        ]
+    }
+];
+
+var recent = [
+    {
+        title: '有社交恐惧症的你，该怎么克服？',
+        cover: 'images/recent/b7ba659ac441c3996e12be395ba61bcb20170501.jpg'
+    },
+    {
+        title: '有社交恐惧症的你，该怎么克服？',
+        cover: 'images/recent/b7ba659ac441c3996e12be395ba61bcb20170501.jpg'
+    },
+    {
+        title: '有社交恐惧症的你，该怎么克服？',
+        cover: 'images/recent/b7ba659ac441c3996e12be395ba61bcb20170501.jpg'
+    },
+    {
+        title: '有社交恐惧症的你，该怎么克服？',
+        cover: 'images/recent/b7ba659ac441c3996e12be395ba61bcb20170501.jpg'
+    },
+    {
+        title: '有社交恐惧症的你，该怎么克服？',
+        cover: 'images/recent/b7ba659ac441c3996e12be395ba61bcb20170501.jpg'
+    }
+];
+
+
+var insterest = [
+
+    {
+        icon: "http://pkimg.image.alimmdn.com/upload/20170414/8dabf2aae99f99cdd598186cbd0380a8.jpg@90w_90h_1e_1c_85Q.jpg",
+        uname: "羊萌萌",
+        desc: "片刻翻唱达人"
+    },
+    {
+        icon: "http://hpimg.pianke.me/4cca2c8e44c978c11697d580db691bb220170209.png?imageView2/2/w/90/format/jpg",
+        uname: "王睿 Ray",
+        desc: "流行歌手王睿"
+    },
+    {
+        icon: "http://pkicon.image.alimmdn.com/icon/876/604876/4940be8b28eee8808afc807251a98355_180",
+        uname: "南夏-",
+        desc: "声音是颗安眠药"
+    },
+    {
+        icon: "http://hpimg.pianke.me/8051fb756aada8fb0ef286a1248e006820170423.jpg?imageView2/2/w/90/format/jpg",
+        uname: "七染和以呆",
+        desc: "酿酒、养猫、讲故事"
+    }
+
+];
